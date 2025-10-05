8a0738298b91fa3296bf80708fdf79af6095a0c7
pabotesu <pab0te2u@gmail.com>
2025-10-05 18:05:36 +0900

    mainからコマンドを参照する

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pabotesu/hulegu/internal/commands/client"
+)
+
+// Version はビルド時に設定されるバージョン情報
+var Version = "0.1.0"
+
+func main() {
+	// バージョン情報を設定
+	client.Version = Version
+
+	// コマンドを実行
+	if err := client.RootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pabotesu/hulegu/internal/commands/server"
+)
+
+// Version はビルド時に設定されるバージョン情報
+var Version = "0.1.0"
+
+func main() {
+	// バージョン情報を設定
+	server.Version = Version
+
+	// コマンドを実行
+	if err := server.RootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
